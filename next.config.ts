51e94c257c310088a78bb5fd7e284d15487c4d8a
Fiko <firuz0643@gmail.com>
2026-09-16 11:03:49 +0400

    Set the right content types for the video stream

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,21 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/video/:file*",
+        source: "/stream/:file*.m4s",
+        headers: [
+          { key: "Content-Type", value: "video/iso.segment" },
+          { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
+        ],
+      },
+      {
+        source: "/stream/:file*.m3u8",
+        headers: [
+          { key: "Content-Type", value: "application/vnd.apple.mpegurl" },
+          { key: "Cache-Control", value: "public, max-age=3600" },
+        ],
+      },
+      {
+        source: "/stream/:file*.mp4",
         headers: [
           { key: "Cache-Control", value: "public, max-age=31536000, immutable" },
         ],
