a415791404cd3c7b4bc8241065e40142d0ba07f7
Fiko <firuz0643@gmail.com>
2026-09-17 20:05:13 +0400

    Put the partner strip above the progress dials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
         <QuoteBanner />
         <SideNavShowcase />
         <Services />
-        <ProgressStats />
         <PartnerStrip />
+        <ProgressStats />
       </main>
     </>
   );
