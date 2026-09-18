5991868df80a10cea8c246de4291ce99fba5d372
Fiko <firuz0643@gmail.com>
2026-09-18 12:05:28 +0400

    Use the real TikTok note and lay the social marks on their side

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -22,18 +22,14 @@ function YoutubeIcon() {
 }
 
 const TIKTOK_GLYPH =
-  "M16.44 0h-3.9v15.67a3.3 3.3 0 0 1-3.3 3.19 3.24 3.24 0 0 1-3.29-3.19 3.3 3.3 0 0 1 3.06-3.2V8.5a7.16 7.16 0 0 0-6.96 7.17A7.13 7.13 0 0 0 9.3 22.8a7.13 7.13 0 0 0 7.25-7.13V7.63a8.9 8.9 0 0 0 5.15 1.64V5.4a5.28 5.28 0 0 1-5.26-5.4z";
+  "M12.53.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z";
 
 function TiktokIcon() {
   return (
-    <svg viewBox="0 0 22 24" className={styles.socialIcon} aria-hidden>
-      {/* the brand's offset copies: cyan behind, red in front, white note on top */}
-      <g transform="translate(-1 1)">
-        <path fill="#25f4ee" d={TIKTOK_GLYPH} />
-      </g>
-      <g transform="translate(1 -1)">
-        <path fill="#fe2c55" d={TIKTOK_GLYPH} />
-      </g>
+    <svg viewBox="0 0 24 24" className={styles.socialIcon} aria-hidden>
+      {/* the mark is one note printed three times: cyan behind, red in front */}
+      <path fill="#25f4ee" d={TIKTOK_GLYPH} transform="translate(-1.1 1.1)" />
+      <path fill="#fe2c55" d={TIKTOK_GLYPH} transform="translate(1.1 -1.1)" />
       <path fill="#fff" d={TIKTOK_GLYPH} />
     </svg>
   );
